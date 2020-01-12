93c754de2bf00e3782f3ac5fa56c72baed6cee0e
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2020-01-12 21:54:43 +0900

    update

--- py/create_figure_ML.py
+++ py/create_figure_ML.py
@@ -9,20 +9,26 @@ import matplotlib.pyplot as plt
 # plt.style.use('seaborn-white')
 plt.style.use('bmh')
 
-from matplotlib import cycler
-colors = cycler('color', ['#EE6666', '#3388BB', '#9988DD', '#EECC55', '#88BB44', '#FFBBBB'])
-plt.rc('axes', facecolor='#E6E6E6', edgecolor='none', axisbelow=True, grid=False, prop_cycle=colors)
-plt.rc('grid', color='w', linestyle='solid')
-plt.rc('patch', edgecolor='#E6E6E6')
-plt.rc('lines', linewidth=2)
+# from matplotlib import cycler
+# colors = cycler('color', ['#EE6666', '#3388BB', '#9988DD', '#EECC55', '#88BB44', '#FFBBBB'])
+# plt.rc('axes', facecolor='#E6E6E6', edgecolor='none', axisbelow=True, grid=False, prop_cycle=colors)
+# plt.rc('grid', color='w', linestyle='solid')
+# plt.rc('patch', edgecolor='#E6E6E6')
+# plt.rc('lines', linewidth=2)
 # plt.figure(figsize=(8,6))
+
+plt.rcParams["mathtext.rm"] = "Times New Roman"
+# plt.rcParams['font.family'] = 'Times New Roman'
+plt.rcParams["mathtext.fontset"] = "cm"
+# plt.rcParams['mathtext.fontset'] = 'stix'
+plt.rcParams["font.size"] = 14
 
 # Check arguments
 import sys
 args = sys.argv
 if len(args) != 2:
-    print("\nUSAGE   : $ python create_figure_M.py [csv_file_path]")
-    print("EXAMPLE : $ python create_figure_M.py OUTPUT/funehoko/SD_mean_max.csv\n")
+    print("\nUSAGE   : $ python create_figure_ML.py [csv_file_path]")
+    print("EXAMPLE : $ python create_figure_ML.py OUTPUT/funehoko/SD_mean_max.csv\n")
     sys.exit()
 
 
@@ -53,11 +59,7 @@ ML     = M_mean / L
 # M/L
 plt.scatter(L, ML, color='black')
 
-# plt.legend(fontsize=14)
 
-plt.rcParams["mathtext.fontset"] = "cm"
-plt.rcParams["mathtext.rm"] = "Times New Roman"
-plt.rcParams["font.size"] = 14
 plt.xlabel('$L$', fontsize=14)
 # plt.ylabel('$M$', fontsize=14) # Gray scale
 plt.ylabel('$M / L$', fontsize=14) # Gray scale
@@ -66,6 +68,7 @@ plt.xticks([2, 50, 100, 150], fontsize=14)
 # plt.yticks([0, 5, 10, 15, 20], fontsize=14) # max
 plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0], fontsize=14) # max
 
-plt.grid()
+# plt.grid()
+# plt.legend(fontsize=14)
 
 plt.show()
